ca80f3b10e0e41498d34408a8187445384028121
Scott G. Daniel <scottdaniel25@gmail.com>
2017-01-27 15:48:09 -0700

    stuff

--- scripts/config.sh
+++ scripts/config.sh
@@ -22,8 +22,6 @@ export KNGFF="$KNOWN/all-PATRIC-CDS.gff"
 
 export UNK="/rsgrps/bhurwitz/scottdaniel/unknown-assembly"
 
-export UNKFASTA="$UNK/prodigal_out/contigs-longer-than-1000.fa-prodigal.fa"
-
 export UNKGFF="$UNK/gff/contigs-longer-than-1000.gff"
 
 PRJ_DIR="/rsgrps/bhurwitz/scottdaniel/extract-fasta"
